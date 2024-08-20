3b6607d77121c16cbbd3e484522b87e86a668fa6
Akshayaa B K <116330039+Akshayaa-bk@users.noreply.github.com>
2024-08-20 23:09:44 +0530

    Update __init__.py

--- url_app/__init__.py
+++ url_app/__init__.py
@@ -7,14 +7,19 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sqlite.db"
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    app.config['DEBUG'] = True  # Enable debug mode
+
     db.init_app(app)
 
+    # Register blueprints
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    with app.app_context():
+        db.create_all()  # Creates tables if they don't exist
 
     return app
