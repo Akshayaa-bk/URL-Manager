9b83a4721a5180346628533d60088a5e8f79859d
Akshayaa B K <116330039+Akshayaa-bk@users.noreply.github.com>
2024-08-20 23:11:04 +0530

    Update models.py

--- url_app/models.py
+++ url_app/models.py
@@ -1,14 +1,7 @@
-from datetime import datetime
 from . import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(1000), nullable=False)
-    # Optional: Add any additional fields you might need, like timestamps for when the user was created or last modified
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
