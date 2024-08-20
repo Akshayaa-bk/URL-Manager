5d7ffd5b351c5833b5eaddf43f4631e61b4dd34e
Akshayaa B K <116330039+Akshayaa-bk@users.noreply.github.com>
2024-08-20 23:10:26 +0530

    Update auth.py

--- url_app/auth.py
+++ url_app/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, url_for, request , redirect
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+from werkzeug.security import generate_password_hash
+from .models import User
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -11,6 +15,16 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        print("User Already exists!")
+    
+    new_user = User(email=email, name= name, password = generate_password_hash(password, method ='pbkdf2:sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
     return redirect(url_for('auth.login'))
 
 
